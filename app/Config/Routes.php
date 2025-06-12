6a82abb6093134362f04f0c44aee963e4cafc236
Atsy354 <aksiomayufuna@gmail.com>
2025-06-12 23:53:41 +0700

    update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -17,6 +17,7 @@ $routes->group('produk', ['filter' => 'auth'], function ($routes) {
     $routes->post('', 'ProdukController::create');
     $routes->post('edit/(:any)', 'ProdukController::edit/$1');
     $routes->get('delete/(:any)', 'ProdukController::delete/$1');
+    $routes->get('download', 'ProdukController::download');
 });
 
 $routes->group('produkkategori', ['filter' => 'auth'], function ($routes) { 
@@ -26,6 +27,12 @@ $routes->group('produkkategori', ['filter' => 'auth'], function ($routes) {
     $routes->get('delete/(:any)', 'ProdukKategoriController::delete/$1');
 });
 
-$routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
+$routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
+    $routes->get('', 'TransaksiController::index');
+    $routes->post('', 'TransaksiController::cart_add');
+    $routes->post('edit', 'TransaksiController::cart_edit');
+    $routes->get('delete/(:any)', 'TransaksiController::cart_delete/$1');
+    $routes->get('clear', 'TransaksiController::cart_clear');
+});
 
 $routes->get('contact', 'ContactController::index', ['filter' => 'auth']);
